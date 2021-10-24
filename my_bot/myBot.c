e5a585d773ee1b2cad3d72f96ee7faea71b91e7f
straight-into-the-wall <92807553+straight-into-the-wall@users.noreply.github.com>
2021-10-24 15:20:17 +0200

    feat: simple first discord bot

--- my_bot/myBot.c
+++ my_bot/myBot.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
+
 #include "discord.h"
 
-
-void on_ready(
-  struct discord *client, 
-  const struct discord_user *bot) 
+void on_ready(struct discord* client, const struct discord_user* bot)
 {
   log_info("Logged in as %s!", bot->username);
 }
 
-int main() {
-  struct discord *client = discord_config_init("../config.json");
+void log_msg(const struct discord_message* msg)
+{
+  log_info("MSG received on %lld from %s content: %s |", msg->channel_id,
+           msg->author->username, msg->content);
+}
+
+void on_bum(struct discord* client, const struct discord_user* bot,
+            const struct discord_message* msg)
+{
+  if (msg->author->bot) return; // ignore bots
+
+  struct discord_create_message_params params = { .content = "crash" };
+  discord_create_message(client, msg->channel_id, &params, NULL);
+  log_msg(msg);
+}
+
+void on_crash(struct discord* client, const struct discord_user* bot,
+              const struct discord_message* msg)
+{
+  if (msg->author->bot) return; // ignore bots
+
+  struct discord_create_message_params params = { .content = "bum" };
+  discord_create_message(client, msg->channel_id, &params, NULL);
+  log_msg(msg);
+}
+
+int main()
+{
+  struct discord* client = discord_config_init("./mybot_config.json");
 
   discord_set_on_ready(client, &on_ready);
+  discord_set_on_command(client, "bum", &on_bum);
+  discord_set_on_command(client, "crash", &on_crash);
+
   discord_run(client);
 
   discord_cleanup(client);
